bc0ca5ce374b56faf390f0ea3b28318b548ecdb1
Oliver Lindemann <lindemann09@googlemail.com>
2023-07-14 20:19:56 +0200

    update

--- scripts/install_requirements.R
+++ scripts/install_requirements.R
@@ -1,2 +1,5 @@
+
+install.packages("base64enc", "knitr", "rmarkdown", "magick",
+            "openxlsx", "png", "RCurl", "RJSONIO", "tinytex", "xml2")
 install.packages("exams", repos = "http://R-Forge.R-project.org",
                 dependencies = TRUE, type = "source")
